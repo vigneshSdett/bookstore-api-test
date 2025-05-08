7711ef8f3320d142b4efa007c6506a90cc26209c
svignesharunkumar <svignesharunkumar@gmail.com>
2025-05-09 01:41:35 +0530

    master Automation-cicd

---
+++ src/test/java/com/bookstore/runners/TestRunner.java
@@ -0,0 +1,15 @@
+package com.bookstore.runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"com.bookstore.stepdefs", "com.bookstore.hooks"},
+    plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
+    monochrome = true
+)
+public class TestRunner {
+}
